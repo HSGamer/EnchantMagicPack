9c4c7b8c7cf5e38b4b68c6440685312b1cb2ac2e
HSGamer <huynhqtienvtag@gmail.com>
2018-11-09 17:54:49 +0700

    New Enchant : GravityArrow
    Signed-off-by: HSGamer <huynhqtienvtag@gmail.com>

---
+++ src/main/java/me/hsgamer/enchantmagicpack/enchants/projectile/shoot/GravityArrow.java
@@ -0,0 +1,18 @@
+package me.hsgamer.enchantmagicpack.enchants.projectile.shoot;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class GravityArrow extends ProjectileShoot {
+    public GravityArrow() {
+        super("GravityArrow", "Arrows will be no gravity when shoot");
+
+        setMaxLevel(1, 1);
+        setWeight(1);
+    }
+
+    public void applyProjectileShoot(LivingEntity user, ItemStack bow, int level, EntityShootBowEvent event) {
+        event.getProjectile().setGravity(false);
+    }
+}
