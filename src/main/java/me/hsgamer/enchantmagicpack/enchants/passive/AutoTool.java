74da68db85eee2f1187e9893af5da4c807584634
hsgamer <huynhqtienvtag@gmail.com>
2018-12-08 21:04:13 +0700

    AutoTool: use XMaterial on checking

--- src/main/java/me/hsgamer/enchantmagicpack/enchants/passive/AutoTool.java
+++ src/main/java/me/hsgamer/enchantmagicpack/enchants/passive/AutoTool.java
@@ -26,28 +26,28 @@ public class AutoTool extends CustomEnchantment {
     public void applyInteractBlock(Player user, int level, PlayerInteractEvent event) {
         if (event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             XMaterial blocktype = XMaterial.fromString(event.getClickedBlock().getType().toString());
-            String itemtype = event.getMaterial().toString();
+            String itemtype = XMaterial.fromString(event.getMaterial().toString()).toString();
             String value;
             if (itemtype.startsWith("DIAMOND_")) {
                 value = "DIAMOND_";
-            } else if (itemtype.startsWith("GOLD_")) {
-                value = "GOLD_";
+            } else if (itemtype.startsWith("GOLDEN_")) {
+                value = "GOLDEN_";
             } else if (itemtype.startsWith("IRON_")) {
                 value = "IRON_";
             } else if (itemtype.startsWith("STONE_")) {
                 value = "STONE_";
-            } else if (itemtype.startsWith("WOOD_")) {
-                value = "WOOD_";
+            } else if (itemtype.startsWith("WOODEN_")) {
+                value = "WOODEN_";
             } else return;
 
             if (ToolsUtils.AXES_BLOCKS.contains(blocktype)) {
-                event.getItem().setType(Material.valueOf(value + "AXE"));
+                event.getItem().setType(XMaterial.valueOf(value + "AXE").parseMaterial());
             } else if (ToolsUtils.PICKAXES_BLOCKS.contains(blocktype)) {
-                event.getItem().setType(Material.valueOf(value + "PICKAXE"));
+                event.getItem().setType(XMaterial.valueOf(value + "PICKAXE").parseMaterial());
             } else if (ToolsUtils.SHOVELS_BLOCKS.contains(blocktype)) {
-                event.getItem().setType(Material.valueOf(value + "SPADE"));
+                event.getItem().setType(XMaterial.valueOf(value + "SHOVEL").parseMaterial());
             } else if (ToolsUtils.SWORDS_BLOCKS.contains(blocktype)) {
-                event.getItem().setType(Material.valueOf(value + "SWORD"));
+                event.getItem().setType(XMaterial.valueOf(value + "SWORD").parseMaterial());
             }
         }
     }
@@ -55,19 +55,19 @@ public class AutoTool extends CustomEnchantment {
     public void applyOnHit(LivingEntity user, LivingEntity target, int level, EntityDamageByEntityEvent event) {
         if (!(user instanceof Player)) return;
         ItemStack item = ((Player) user).getInventory().getItemInMainHand();
-        String itemtype = item.getType().toString();
+        String itemtype = XMaterial.fromString(item.getType().toString()).toString();
         String value;
         if (itemtype.startsWith("DIAMOND_")) {
             value = "DIAMOND_";
-        } else if (itemtype.startsWith("GOLD_")) {
-            value = "GOLD_";
+        } else if (itemtype.startsWith("GOLDEN_")) {
+            value = "GOLDEN_";
         } else if (itemtype.startsWith("IRON_")) {
             value = "IRON_";
         } else if (itemtype.startsWith("STONE_")) {
             value = "STONE_";
-        } else if (itemtype.startsWith("WOOD_")) {
-            value = "WOOD_";
+        } else if (itemtype.startsWith("WOODEN_")) {
+            value = "WOODEN_";
         } else return;
-        item.setType(Material.valueOf(value + "SWORD"));
+        item.setType(XMaterial.valueOf(value + "SWORD").parseMaterial());
     }
 }
