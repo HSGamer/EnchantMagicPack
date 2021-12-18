989a9e08571ab8c8fef42e40a9dd35d831186bdc
Huynh Tien <huynhqtienvtag@gmail.com>
2021-12-18 17:58:47 +0700

    more support on AutoTool

--- src/main/java/me/hsgamer/enchantmagicpack/enchants/passive/AutoTool.java
+++ src/main/java/me/hsgamer/enchantmagicpack/enchants/passive/AutoTool.java
@@ -4,9 +4,9 @@ import com.sucy.enchant.api.CustomEnchantment;
 import com.sucy.enchant.api.ItemSet;
 import org.bukkit.Material;
 import org.bukkit.Tag;
+import org.bukkit.block.Block;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -24,51 +24,49 @@ public class AutoTool extends CustomEnchantment {
 
     @Override
     public void applyInteractBlock(Player user, int level, PlayerInteractEvent event) {
-        if (event.getAction() == Action.LEFT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            Material blocktype = event.getClickedBlock().getType();
-            String itemtype = event.getMaterial().name();
-            String value;
-            if (itemtype.startsWith("DIAMOND_")) {
-                value = "DIAMOND_";
-            } else if (itemtype.startsWith("GOLDEN_")) {
-                value = "GOLDEN_";
-            } else if (itemtype.startsWith("IRON_")) {
-                value = "IRON_";
-            } else if (itemtype.startsWith("STONE_")) {
-                value = "STONE_";
-            } else if (itemtype.startsWith("WOODEN_")) {
-                value = "WOODEN_";
-            } else return;
+        Block block = event.getClickedBlock();
+        if (block == null) return;
+        Material blockType = block.getType();
+        String itemType = event.getMaterial().name();
+        String value = getMineralType(itemType);
+        if (value == null) return;
 
-            if (Tag.MINEABLE_AXE.isTagged(blocktype)) {
-                event.getItem().setType(Material.valueOf(value + "AXE"));
-            } else if (Tag.MINEABLE_PICKAXE.isTagged(blocktype)) {
-                event.getItem().setType(Material.valueOf(value + "PICKAXE"));
-            } else if (Tag.MINEABLE_SHOVEL.isTagged(blocktype)) {
-                event.getItem().setType(Material.valueOf(value + "SHOVEL"));
-            } else {
-                event.getItem().setType(Material.valueOf(value + "SWORD"));
-            }
+        if (Tag.MINEABLE_AXE.isTagged(blockType)) {
+            event.getItem().setType(Material.valueOf(value + "AXE"));
+        } else if (Tag.MINEABLE_PICKAXE.isTagged(blockType)) {
+            event.getItem().setType(Material.valueOf(value + "PICKAXE"));
+        } else if (Tag.MINEABLE_SHOVEL.isTagged(blockType)) {
+            event.getItem().setType(Material.valueOf(value + "SHOVEL"));
+        } else if (Tag.MINEABLE_HOE.isTagged(blockType)) {
+            event.getItem().setType(Material.valueOf(value + "HOE"));
+        } else {
+            event.getItem().setType(Material.valueOf(value + "SWORD"));
         }
+    }
+
+    private String getMineralType(String itemType) {
+        if (itemType.startsWith("DIAMOND_")) {
+            return "DIAMOND_";
+        } else if (itemType.startsWith("GOLDEN_")) {
+            return "GOLDEN_";
+        } else if (itemType.startsWith("IRON_")) {
+            return "IRON_";
+        } else if (itemType.startsWith("STONE_")) {
+            return "STONE_";
+        } else if (itemType.startsWith("WOODEN_")) {
+            return "WOODEN_";
+        } else if (itemType.startsWith("NETHERITE_")) {
+            return "NETHERITE_";
+        } else return null;
     }
 
     @Override
     public void applyOnHit(LivingEntity user, LivingEntity target, int level, EntityDamageByEntityEvent event) {
-        if (!(user instanceof Player)) return;
-        ItemStack item = ((Player) user).getInventory().getItemInMainHand();
-        String itemtype = item.getType().name();
-        String value;
-        if (itemtype.startsWith("DIAMOND_")) {
-            value = "DIAMOND_";
-        } else if (itemtype.startsWith("GOLDEN_")) {
-            value = "GOLDEN_";
-        } else if (itemtype.startsWith("IRON_")) {
-            value = "IRON_";
-        } else if (itemtype.startsWith("STONE_")) {
-            value = "STONE_";
-        } else if (itemtype.startsWith("WOODEN_")) {
-            value = "WOODEN_";
-        } else return;
+        if (!(user instanceof Player player)) return;
+        ItemStack item = player.getInventory().getItemInMainHand();
+        String itemType = item.getType().name();
+        String value = getMineralType(itemType);
+        if (value == null) return;
         item.setType(Material.valueOf(value + "SWORD"));
     }
 }
